bf066c8549f9e476311da84b4f127750c1f30bc3
jeaner <jeaner@devdemac-mini.local>
2017-09-12 15:46:23 +0800

    Initial Commit

---
+++ ChangeUI/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ChangeUI
+//
+//  Created by jeaner on 2017/9/12.
+//  Copyright © 2017年 Jeaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
